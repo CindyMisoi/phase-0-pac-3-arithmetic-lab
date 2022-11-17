6421a075b64a483798606881bb1422c7216867ab
CindyMisoi <crmisoi2k@gmail.com>
2022-11-17 19:00:33 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add (a,b) {
+    return a + b;
+};
+add(1,2);
+
+function subtract (a,b) {
+    return a - b;
+};
+subtract(3,1);
+
+function multiply (a,b) {
+    return a * b
+};
+multiply(4,5);
+
+function divide (a,b) {
+    return a / b;
+};
+divide(9,3);
+
+function increment(n){
+ return n += 1;
+};
+increment(5);
+
+function decrement(n){
+ return n -= 1;
+};
+decrement(4);
+
+function makeInt(n){
+return parseInt(n, 10);
+}
+makeInt("2");
+
+function preserveDecimal(n){
+return parseFloat(n);
+}
+preserveDecimal(80.123999);
